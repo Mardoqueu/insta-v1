27fc514882978d709b15ebd7ba8b5c4fadf71bc2
mardoqueu <mdq767@gmail.com>
2022-08-26 10:19:55 -0300

    isntall next-auth  and firebase and initilize the firebase

---
+++ .history/firebase_20220826101145.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: "insta-v1-7054a.appspot.com",
+  messagingSenderId: "5719658762",
+  appId: "1:5719658762:web:49b88d9e0abcf58ca3aaf3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
